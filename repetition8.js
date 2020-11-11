464d37010d652cf4f0e3e9150adfb01a46cf2649
shivamj2603 <shivamj2603@gmail.com>
2020-11-11 09:00:44 +0530

    Added:Program to find magic number

---
+++ repetition8.js
@@ -0,0 +1,30 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+rl.question('Think of a number between 1 and 100(Y/N)', (answer) => {
+  if (answer == 'Y') {
+    checkNum(50);
+  }
+  rl.close();
+});
+var checkNum = function (num) {
+  rl.question('Is number equal to ' + num, function (answer) {
+    if (answer == 'Y') {
+      console.log("Magic number is : " + num);
+      return rl.close(); //closing RL and returning from function.
+    }
+    else if (answer == 'N') {
+      rl.question('Is number greater than ' + num + ' ?(Y/N)', function (answer) {
+        if (answer == 'Y') {
+          checkNum(Math.floor(num + (num / 2)));
+        }
+        else if (answer == 'N') {
+          checkNum(Math.floor(num - (num / 2)));
+        }
+      });
+    }
+    rl.close();
+  });
+}
